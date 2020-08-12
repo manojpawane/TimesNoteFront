31e033283e19df659ce92edc09be343508250ccf
manojpawane <manojpawane@gmail.com>
2020-08-12 12:01:07 +0530

    Confirm post added for front end

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { HttpClient } from '@angular/common/http';
 import { HttpRequestService } from 'src/app/services/registration.service';
-
+import * as jwt_decode from 'jwt-decode';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,9 +10,10 @@ import { HttpRequestService } from 'src/app/services/registration.service';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  constructor(private fb:FormBuilder, private service:HttpRequestService) { }
+  res;
+  constructor(private fb: FormBuilder, private service: HttpRequestService) { }
 
-  
+
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
@@ -20,24 +21,27 @@ export class LoginComponent implements OnInit {
       password: ['', Validators.required]
     })
   }
-  get email(){
+  get email() {
     return this.loginForm.get('email')
   }
-  get password(){
+  get password() {
     return this.loginForm.get('password')
   }
 
-login(){
-  this.service.login(
-    this.loginForm.value
-).subscribe(res=>{
-    console.log(res);
-    
-},err=>{
-console.log(err);
+  login() {
+    this.service.login(
+      this.loginForm.value
+    ).subscribe(res => {
+      console.log('test:')
+      console.log(res);
+      this.res = res;
+      var decoded = jwt_decode(this.res.token);
+      console.log(decoded);
+    }, err => {
+      console.log(err);
 
-});
-}
+    });
+  }
 }
 
 
